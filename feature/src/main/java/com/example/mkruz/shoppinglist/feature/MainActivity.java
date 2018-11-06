5bb670b806eda15504ee942698391c8e1bc652d2
Marcin-Kruzewski <m.kruzewski@outlook.com>
2018-11-06 21:49:32 +0100

    Consider as done

--- feature/src/main/java/com/example/mkruz/shoppinglist/feature/MainActivity.java
+++ feature/src/main/java/com/example/mkruz/shoppinglist/feature/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.mkruz.shoppinglist.feature;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -27,10 +32,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String language = prefs.getString("example_text", "");
+        setTitle(language);
 
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
             getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimaryDark));
+            if (prefs.getString("dark_theme", "").equals("2")){
+                Toast.makeText(getApplicationContext(), "Dark theme", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
@@ -47,10 +57,10 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent intent2 = new Intent(this, SettingsActivity.class);
+            startActivity(intent2);
         }
 
         return super.onOptionsItemSelected(item);
